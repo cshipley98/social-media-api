6946330b4add56a6c2de19f43496f2f28bc904ae
cshipley98 <claireshipley98@gmail.com>
2024-05-21 08:48:08 -0600

    mongo db

--- server.js
+++ server.js
@@ -14,3 +14,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-me
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
